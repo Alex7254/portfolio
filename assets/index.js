d7a7951ede973002f123b7767ce028a41b69e464
Alejandro <alejandro199898@gmail.com>
2025-02-18 10:46:53 -0800

    work commit

---
+++ assets/index.js
@@ -0,0 +1,64 @@
+const setTheme = (theme) => {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+}
+
+const getPreferredTheme = () => {
+  const storedTheme = localStorage.getItem('theme');
+  if (storedTheme) {
+    return storedTheme;
+  }
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+}
+
+const applyTheme = () => {
+  setTheme(getPreferredTheme());
+}
+
+const toggleTheme = () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  const icons = document.querySelectorAll('.theme-icon');
+
+  // Loop through the icons and toggle the "display-none" class
+  icons.forEach(icon => {
+    icon.classList.toggle('display-none');
+  });
+  setTheme(newTheme);
+}
+
+// Apply theme on page load
+applyTheme();
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggleButton = document.getElementById('themeToggle');
+
+  themeToggleButton.addEventListener('click', toggleTheme);
+
+  // Accordion
+  let acc = document.getElementsByClassName("accordion");
+  let i;
+
+  for (i = 0; i < acc.length; i++) {
+
+    acc[i].addEventListener("click", function () {
+      let panel = this.nextElementSibling;
+      this.classList.toggle("active");
+      panel.classList.toggle("active");
+    });
+  }
+
+  window.addEventListener('load', function (event) {
+    if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+      // Page was refreshed
+      console.log("Page was refreshed");
+      // toggleTheme();
+    }
+  });
+});
+
